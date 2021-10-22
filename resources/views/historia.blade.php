ccff8546ff078db614687116455b24a63e4ce5af
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2021-10-21 22:00:26 -0300

    primera subida

---
+++ resources/views/historia.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h1 class="font-bold text-x1 ">Historia</h1>
+    </x-slot>
+    <div class="py-12">
+        <div class="ml-16">
+            <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-x1 sm:rounded-lg">
+                    <p>la escuela maria teresa marchant fue fundada en el año ni idea</p>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-x1 sm:rounded-lg">
+                <p>la escuela maria teresa marchant fue fundada en el año ni idea</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
+    
