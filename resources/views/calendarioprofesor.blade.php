2a6a7c2b9855d5b5eae9ab72ad62a97db199dcc9
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-01 20:58:49 -0300

    calendario 2.1

--- resources/views/calendarioprofesor.blade.php
+++ resources/views/calendarioprofesor.blade.php
@@ -4,20 +4,20 @@
 ?>
 @extends('layouts.userprofesor')
 @section('Content')
-    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.2/locales-all.js"></script>
-    <script>
-      document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
-        });
-        calendar.render();
-      });
-    </script>
     <div id='calendar'>
 
     </div>
 @endsection
 <style>
 </style>
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.2/locales-all.js"></script>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth'
+        });
+        calendar.render();
+    });
+</script>
