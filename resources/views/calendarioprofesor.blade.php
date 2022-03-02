2c18e2cd4e96a69bd427aaef62582081b0d0aceb
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-01 21:03:54 -0300

    calendario 2.02

--- resources/views/calendarioprofesor.blade.php
+++ resources/views/calendarioprofesor.blade.php
@@ -9,9 +9,7 @@
     </div>
 @endsection
 <style>
-    #calendar{
-        margin: 25%;
-    }
+
 </style>
 <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.2/locales-all.js"></script>
