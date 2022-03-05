2df19f169ac69d575e9eb9ee776f711d69fcebba
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-05 13:29:21 -0300

    calendario 3.1

--- resources/views/calendarioprofesor.blade.php
+++ resources/views/calendarioprofesor.blade.php
@@ -63,11 +63,7 @@
                 alert('Clicked on: ' + date.format());
                 alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
                 alert('Current view: ' + view.name);
-                // change the day's background color just for fun
-                $(this).css('background-color', 'red');
-        
-                //------------Llamando al modal de Bootstrap
-                $("#evaluaciones").modal();
+                
             }
         });
         calendar.render();
