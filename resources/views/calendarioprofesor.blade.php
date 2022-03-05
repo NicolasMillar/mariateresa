4d2804f685d929018f96030b38dadb92a484832d
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-05 16:16:23 -0300

    calendario 4.3

--- resources/views/calendarioprofesor.blade.php
+++ resources/views/calendarioprofesor.blade.php
@@ -7,10 +7,8 @@
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
-@extends('layouts.userprofesor')
+
+
 @section('Content')
 
 <body>
@@ -66,13 +64,10 @@
             },
             dateClick: function(info) {
                 alert('Clicked on: ' + info.dateStr);
-                myFunction();
+                $("#exampleModal").modal("show");
             }
         });
         calendar.setOption('height', 700);
         calendar.render();
-        function myFunction() {
-            $("#exampleModal").modal("show");
-        }
     });
 </script>
