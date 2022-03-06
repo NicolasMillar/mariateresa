07254bc580601edaec9764ecacd65959f488be29
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-06 17:34:18 -0300

    calendario 4.3

--- resources/views/calendarioprofesor.blade.php
+++ resources/views/calendarioprofesor.blade.php
@@ -19,9 +19,21 @@
          </div>
          <div class="modal-body">
            <form action="">
-             <section>
-               <option selected="selected" >Seleccione un curso</option>
-             </section>
+             <select name="Curso" id="curso">
+               <option selected="selected"> Seleccione un curso</option>
+             </select>
+             <select name="Semestre" id="Semestre">
+               <option value="1">1</option>
+               <option value="2">2</option>
+               <option value="3">3</option>
+               <option value="4">4</option>
+               <option value="5">5</option>
+               <option value="6">6</option>
+               <option value="7">7</option>
+               <option value="8">8</option>
+               <option value="9">9</option>
+               <option value="10">10</option>
+             </select>
              <input type="text" name="" id="">
            </form>
          </div>
