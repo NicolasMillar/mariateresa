e6baed11626576e8ce2b30323f722b6b3878e652
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-08 13:35:06 -0300

    notas v3.05

--- resources/views/admin/calificaciones/profesor.blade.php
+++ resources/views/admin/calificaciones/profesor.blade.php
@@ -13,13 +13,13 @@
 @section('Content')
     <div style="width: 100%;">
         <div style="width: 100%">
-            <div style="float:right; width:20% ">
+            <div style="float:right; width:15% ">
                 <button style="background-color:rgba(62, 62, 248, 0.664) " id="boton">Crear Evaluacion</button>
             </div>
-            <div style="float: right; width:20% ">
+            <div style="float: right; width:15% ">
                 <button style="background-color:cadetblue " id="boton">Ingresar calificacion</button>
             </div>
-            <div style="float: right; width:20% ">
+            <div style="float: right; width:15% ">
                 <button style="background-color:darkolivegreen" id="boton">Modifica calificacion</button>
             </div>        
         </div>
