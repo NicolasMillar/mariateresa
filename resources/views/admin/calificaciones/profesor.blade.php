6e4c3d9b56111f2ab5286b6138cdb908dddade1a
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-08 13:51:58 -0300

    notas v3.06

--- resources/views/admin/calificaciones/profesor.blade.php
+++ resources/views/admin/calificaciones/profesor.blade.php
@@ -51,27 +51,26 @@
     
 
     <!-- Crear evaluacion -->
-   <div class="modal fade" id="Crearevaluacion" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+    <div class="modal fade" id="Crearevaluacion" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Crear Nueva Evaluacion</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h6>Fecha de la evaluacion</h6>
+                    <h6>Descripcion de la evaluacion</h6>
+                    <input type="text" name="" id="">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
         </div>
-        <div class="modal-body">
-            <h6>Fecha de la evaluacion</h6>
-
-            <h6>Descripcion de la evaluacion</h6>
-            <input type="text" name="" id="">
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
   </div>
 @endsection
 <style>
