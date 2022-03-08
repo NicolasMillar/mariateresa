6cbf2fa65ce6460befa3cfafebabba5a885d96a0
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-08 15:20:23 -0300

    notas v4.02

--- app/Http/Controllers/CalificacionController.php
+++ app/Http/Controllers/CalificacionController.php
@@ -21,7 +21,7 @@ class CalificacionController extends Controller
     {
         $pruebas = Prueba::where('ID_Asignatura', '=', $asignatura->id)->get();
         $notas = DB::table('calificaciones')->join('pruebas', 'pruebas.id', '=', 'calificaciones.ID_Pruebas')->whereIn('ID_Pruebas', $pruebas->pluck('id'))->get();
-        return view('admin.calificaciones.profesor', compact('notas'));
+        return view('admin.calificaciones.profesor', compact('notas','asignatura'));
     }
     
 
