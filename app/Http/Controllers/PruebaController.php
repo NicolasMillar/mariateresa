7edf050db4550710af7e976815946c70ecf18875
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-08 15:43:07 -0300

    notas v4.02

--- app/Http/Controllers/PruebaController.php
+++ app/Http/Controllers/PruebaController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class PruebaController extends Controller
 {
     public function registrar(Asignatura $asignatura){
-        console_log($asignatura->id);
+        json_encode($asignatura->id);
     }
 }
