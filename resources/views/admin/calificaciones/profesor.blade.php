0aac4c7669540d6c6b3980fe5105fcff80399a7c
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-08 19:59:46 -0300

    notas v4.02

--- resources/views/admin/calificaciones/profesor.blade.php
+++ resources/views/admin/calificaciones/profesor.blade.php
@@ -81,7 +81,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary" [disabled]="loginForm.form.invalid" >Guardar cambios</button>
+                    <button type="submit" class="btn btn-primary" {{ (!is_null($Descripcion) && !empty($Descripcion)) ? '' : 'disabled' }} >Guardar cambios</button>
                     {!! Form::close() !!}
                 </div>
             </div>
