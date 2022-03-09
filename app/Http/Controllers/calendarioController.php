71909fe19547db67ad1e4d95f17d10bef10734ae
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-08 23:30:44 -0300

    calendario v5.03

--- app/Http/Controllers/calendarioController.php
+++ app/Http/Controllers/calendarioController.php
@@ -6,13 +6,14 @@ use Session;
 class calendarioController extends Controller{
 
     public function mostrarprofesor(Prueba $p){
+        echo("pase por aca");
         $pruebas= Prueba::all();
         $cantidad=count($pruebas);
         for($i=0;$i<$cantidad;$i++){
             $title="title: '".$pruebas[$i]->Nombre_Prueba."'";
             $star="start: '".$pruebas[$i]->Fecha_Prueba."'";
         }
-        $prue=$title.",".$star;
+        $prue=$title.", ".$star;
         return response()->json($prue);
     }
 
