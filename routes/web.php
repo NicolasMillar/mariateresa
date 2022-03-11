76195ce2ed7ba0d91d706604b4bc0e8bbc890d63
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-11 11:48:41 -0300

    merge

--- routes/web.php
+++ routes/web.php
@@ -123,3 +123,4 @@ Route::get('profesorhome/calendarioprofesor', 'App\Http\Controllers\AlumnoProfes
 Route::get('alumnohome/calendarioalumno', 'App\Http\Controllers\AlumnoProfesor@calendario')->name('calendarioalumno');
 Route::get('profesorhome/calendarioprofesor/mostrar/','App\Http\Controllers\calendarioController@mostrarprofesor')->name('mostrarprofesor');
 Route::get('alumnohome/calendarioalumno/mostrar','App\Http\Controllers\calendarioController@mostraralumno')->name('mostraralumno');
+
