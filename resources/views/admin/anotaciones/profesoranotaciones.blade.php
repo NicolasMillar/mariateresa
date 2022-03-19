d5b75b570f52a9951a1041f0e6d2b9b1e38369c9
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-19 15:44:39 -0300

    inicio de anotaciones v3.3

--- resources/views/admin/anotaciones/profesoranotaciones.blade.php
+++ resources/views/admin/anotaciones/profesoranotaciones.blade.php
@@ -9,7 +9,6 @@
 
 @extends('layouts.userprofesor')
 @section('Content')
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">           
