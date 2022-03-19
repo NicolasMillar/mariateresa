f5ac0fe2da8835241b509d1f4e83be39b3824eb6
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-03-19 16:13:25 -0300

    inicio de anotaciones v3.5

--- resources/views/admin/anotaciones/profesoranotaciones.blade.php
+++ resources/views/admin/anotaciones/profesoranotaciones.blade.php
@@ -12,8 +12,8 @@
     <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">           
-    <div style="text-align:center;" >
-        <table id="tabla" class="table table-striped table-bordered table-sm" style="width: 75%; margin: 0 auto;">
+    <div style=" justify-content: center; display:flex;" >
+        <table id="tabla" class="table table-striped table-bordered table-sm" style="width: 100%">
             <thead>
                 <tr>
                     <th>Rut</th>
