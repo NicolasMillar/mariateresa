bb5bf2ca6f8f2acfe88362a2fcb54e595553e23b
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-09-03 20:22:05 -0400

    diseño admin profesor

--- resources/views/admin/usuario_profesores/create.blade.php
+++ resources/views/admin/usuario_profesores/create.blade.php
@@ -124,16 +124,13 @@
 @endsection
 <style>
 </style>
-    <script type="text/javascript">
-
-        var i = 0;
-
-        $("#add").click(function(){
-            ++i;
-            $("#dynamicTable").append('<tr><td>{!! Form::text("addmore['+i+']", null, ["placeholder"=>"Ingrese los titulos que posee", "class"=>"form-control"]) !!}</td><td><button type="button" class="btn btn-danger remove-tr">Remove</button></td></tr>');
-        });
-
-        $(document).on('click', '.remove-tr', function(){  
-            $(this).parents('tr').remove();
-        });  
-    </script>
+<script type="text/javascript">
+    var i = 0;
+    $("#add").click(function(){
+        ++i;
+        $("#dynamicTable").append('<tr><td>{!! Form::text("addmore['+i+']", null, ["placeholder"=>"Ingrese los titulos que posee", "class"=>"form-control"]) !!}</td><td><button type="button" class="btn btn-danger remove-tr">Remove</button></td></tr>');
+    });
+    $(document).on('click', '.remove-tr', function(){  
+        $(this).parents('tr').remove();
+    });  
+</script>
