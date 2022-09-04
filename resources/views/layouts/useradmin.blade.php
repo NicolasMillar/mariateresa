28a7db6957100a4ab447cd01392cd02393e87fa8
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-09-04 18:44:54 -0300

    diseño admin cursos

--- resources/views/layouts/useradmin.blade.php
+++ resources/views/layouts/useradmin.blade.php
@@ -71,7 +71,7 @@
                 <div class="w3-sidebar w3-bar-block w3-card" style="width:15%; font-size:175%; background-color:#2caae1; color:#ffffff">
                   <a href="{{route('usuario_profesor')}}" class="w3-bar-item w3-button w3-border-bottom">Profesor</a>     
                   <a href="{{route('usuario_alumno')}}" class="w3-bar-item w3-button w3-border-bottom">Alumno</a> 
-                  <a href="{{route('cuenta')}}" class="w3-bar-item w3-button w3-border-bottom">Curso</a>
+                  <a href="{{route('Cursos')}}" class="w3-bar-item w3-button w3-border-bottom">Curso</a>
                   <a href="{{route('cuenta')}}" class="w3-bar-item w3-button w3-border-bottom">Asignatura</a>
                 </div>
               </div>
