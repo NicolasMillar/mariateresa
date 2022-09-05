780f04238c6a0e656b7a9df06b198788b0761e95
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-09-05 16:09:26 -0300

    errores calificaciones

--- app/Http/Controllers/CalificacionController.php
+++ app/Http/Controllers/CalificacionController.php
@@ -29,7 +29,7 @@ class CalificacionController extends Controller
         $anterior=0; 
         $promedios=[];
         $id=0;
-        echo $limite;
+        echo $cont;
         /*for($i=0;$i<$limite;$i++){
             if($anterior != $notas[$i]->ID_Pruebas){
                 $anterior=$notas[$i]->ID_Pruebas;
