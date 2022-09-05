efdda788c749e6edee516de9bae1714472b16681
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-09-04 21:02:59 -0300

    restricciones calificaiones

--- app/Http/Controllers/CalificacionController.php
+++ app/Http/Controllers/CalificacionController.php
@@ -78,7 +78,7 @@ class CalificacionController extends Controller
     public function ActualizarNotasa(Request $request){
         $pruebas= Prueba::where('ID_Asignatura', '=', $request->asignatura)->get();
         for($i=0; $i<count($pruebas);$i++){
-            echo $i;
+            echo $pruebas[$i];
             /*$iden=$i+1;
             $nota=$request->$iden;
             if(!is_numeric($nota)){
